ed532c3e8b9f332962e2d11305dd0e9fcc44a343
sidsudsbucse506 <sichhabra@cs.stonybrook.edu>
2018-05-02 13:18:13 -0400

    Working like a charm.

--- src/CirclePanel.java
+++ src/CirclePanel.java
@@ -8,7 +8,6 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 
 import javax.swing.JPanel;
 
@@ -27,7 +26,7 @@ public class CirclePanel extends JPanel {
 			public void mousePressed(MouseEvent e) {
 				Point p = new Point(e.getX(), e.getY());
 				PointList.points.add(p);
-				State.currentCircle = updateCircle(PointList.points, p);
+				State.currentCircle = CircleUtil.updateCircle(PointList.points, p);
 				Component component[] = CirclePanel.this.getParent().getComponents();
 				Arrays.sort(component, new Comparator<Component>() {
 					@Override
@@ -40,111 +39,6 @@ public class CirclePanel extends JPanel {
 				}
 			}
 		});
-	}
-
-	public Circle updateCircle(List<Point> points, Point p) {
-
-		if (State.currentCircle != null && State.currentCircle.contains(p)) {
-			NotificationMessage.index = 0;
-			return State.currentCircle;
-		}
-		NotificationMessage.index = 1;
-		Circle c = null;
-		for (int i = 0; i < points.size(); i++) {
-			Point point = points.get(i);
-			if (c == null || (!c.contains(point))) {
-				State.p1 = point;
-				c = makeCircleOnePoint(points.subList(0, i + 1), p);
-			}
-		}
-
-		return c;
-	}
-
-	private static Circle makeCircleOnePoint(List<Point> points, Point p) {
-		Circle c = new Circle(p, 0);
-		for (int i = 0; i < points.size(); i++) {
-			Point q = points.get(i);
-			if (!c.contains(q)) {
-				if (c.radius == 0D) {
-					State.p1 = p;
-					State.p2 = q;
-					c = makeDiameter(p, q);
-				}
-				else {
-					State.p1 = p;
-					State.p2 = q;
-					c = makeCircleTwoPoints(points.subList(0, i + 1), p, q);
-				}
-			}
-		}
-		return c;
-	}
-
-	static Circle makeDiameter(Point a, Point b) {
-		Point c = new Point((a.x + b.x) / 2, (a.y + b.y) / 2);
-		return new Circle(c, Math.max(c.distance(a), c.distance(b)));
-	}
-
-	private static Circle makeCircleTwoPoints(List<Point> points, Point p, Point q) {
-		Circle circ = makeDiameter(p, q);
-		State.p1 = p;
-		State.p2 = q;
-		Circle left = null;
-		Circle right = null;
-
-		// For each point not in the two-point circle
-		Point pq = q.subtract(p);
-		Point l_r = null, r_r = null;
-		for (Point r : points) {
-			if (circ.contains(r))
-				continue;
-
-			// Form a circumcircle and classify it on left or right side
-			double cross = pq.cross(r.subtract(p));
-			Circle c = CircleUtil.getCircle(p, q, r);
-			State.p1 = p;
-			State.p2 = q;
-			State.p3 = r;
-			if (c == null)
-				continue;
-			else if (cross > 0
-					&& (left == null || pq.cross(c.center.subtract(p)) > pq.cross(left.center.subtract(p)))) {
-				left = c;
-				l_r = r;
-			} else if (cross < 0
-					&& (right == null || pq.cross(c.center.subtract(p)) < pq.cross(right.center.subtract(p)))) {
-				right = c;
-				r_r = r;
-			}
-		}
-		
-		// Select which circle to return
-		if (left == null && right == null) {
-			return circ;
-		} else if (left == null) {
-			State.p1 = p;
-			State.p2 = q;
-			State.p3 = r_r;
-			return right;
-		} else if (right == null) {
-			State.p1 = p;
-			State.p2 = q;
-			State.p3 = l_r;
-			return left;
-		} else {
-			if (left.radius <= right.radius) {
-				State.p1 = p;
-				State.p2 = q;
-				State.p3 = l_r;
-				return left;
-			} else {
-				State.p1 = p;
-				State.p2 = q;
-				State.p3 = r_r;
-				return right;
-			}
-		}
 	}
 
 	@Override
--- src/CircleUtil.java
+++ src/CircleUtil.java
@@ -1,20 +1,117 @@
+import java.util.List;
+
 public class CircleUtil {
 
-	public static Circle getCircle(Point a, Point b, Point c) {
-		// Mathematical algorithm from Wikipedia: Circumscribed circle
-		double ox = (Math.min(Math.min(a.x, b.x), c.x) + Math.max(Math.min(a.x, b.x), c.x)) / 2;
-		double oy = (Math.min(Math.min(a.y, b.y), c.y) + Math.max(Math.min(a.y, b.y), c.y)) / 2;
-		double ax = a.x - ox, ay = a.y - oy;
-		double bx = b.x - ox, by = b.y - oy;
-		double cx = c.x - ox, cy = c.y - oy;
-		double d = (ax * (by - cy) + bx * (cy - ay) + cx * (ay - by)) * 2;
-		if (d == 0)
+	public static Circle getCircle(Point p1, Point p2, Point p3) {
+
+		double a = p2.getX() - p1.getX();
+		double b = p2.getY() - p1.getY();
+		double c = p3.getX() - p1.getX();
+		double d = p3.getY() - p1.getY();
+
+		double e = a * (p1.getX() + p2.getX()) + b * (p1.getY() + p2.getY());
+		double f = c * (p1.getX() + p3.getX()) + d * (p1.getY() + p3.getY());
+		double g = 2 * (a * (p3.getY() - p2.getY()) - b * (p3.getX() - p2.getX()));
+
+		if (g == 0)
 			return null;
-		double x = ((ax * ax + ay * ay) * (by - cy) + (bx * bx + by * by) * (cy - ay) + (cx * cx + cy * cy) * (ay - by)) / d;
-		double y = ((ax * ax + ay * ay) * (cx - bx) + (bx * bx + by * by) * (ax - cx) + (cx * cx + cy * cy) * (bx - ax)) / d;
-		Point p = new Point(ox + x, oy + y);
-		double r = Math.max(Math.max(p.distance(a), p.distance(b)), p.distance(c));
-		return new Circle(p, r);
+
+		double centerX = (d * e - b * f) / g;
+		double centerY = (a * f - c * e) / g;
+		double radius = Math
+				.sqrt((p1.getX() - centerX) * (p1.getX() - centerX) + (p1.getY() - centerY) * (p1.getY() - centerY));
+		Circle circle = new Circle(new Point(centerX, centerY), radius);
+		return circle;
+
+	}
+	
+	public static Circle updateCircle(List<Point> points, Point p) {
+
+		if (State.currentCircle != null && State.currentCircle.contains(p)) {
+			NotificationMessage.index = 0;
+			return State.currentCircle;
+		}
+		NotificationMessage.index = 1;
+		Circle c = new Circle(p, 0);
+
+		if (!c.contains(p) && c.radius == 0D) {
+			c = CircleUtil.getCircle(c.center, p);
+		} else {
+			for (int i = 0; i < points.size(); i++) {
+				Point q = points.get(i);
+				if (!c.contains(q)) {
+					if (c.radius == 0D) {
+						c = CircleUtil.getCircle(p, q);
+					} else {
+						c = randomizedIncremental(points.subList(0, i + 1), p, q);
+					}
+				}
+			}
+		}
+		return c;
+	}
+
+	private static Circle randomizedIncremental(List<Point> points, Point p, Point q) {
+		Circle circ = CircleUtil.getCircle(p, q);
+		State.p1 = p;
+		State.p2 = q;
+		Circle left = null;
+		Circle right = null;
+
+		Point pq = q.subtract(p);
+		Point l_r = null, r_r = null;
+		for (Point r : points) {
+			if (circ.contains(r))
+				continue;
+
+			double cross = pq.cross(r.subtract(p));
+			Circle c = CircleUtil.getCircle(p, q, r);
+			State.p1 = p;
+			State.p2 = q;
+			State.p3 = r;
+			if (c == null)
+				continue;
+			else if (cross > 0
+					&& (left == null || pq.cross(c.center.subtract(p)) > pq.cross(left.center.subtract(p)))) {
+				left = c;
+				l_r = r;
+			} else if (cross < 0
+					&& (right == null || pq.cross(c.center.subtract(p)) < pq.cross(right.center.subtract(p)))) {
+				right = c;
+				r_r = r;
+			}
+		}
+
+		if (left == null && right == null) {
+			return circ;
+		} else if (left == null) {
+			State.p1 = p;
+			State.p2 = q;
+			State.p3 = r_r;
+			return right;
+		} else if (right == null) {
+			State.p1 = p;
+			State.p2 = q;
+			State.p3 = l_r;
+			return left;
+		} else {
+			if (left.radius <= right.radius) {
+				State.p1 = p;
+				State.p2 = q;
+				State.p3 = l_r;
+				return left;
+			} else {
+				State.p1 = p;
+				State.p2 = q;
+				State.p3 = r_r;
+				return right;
+			}
+		}
+	}
+
+	public static Circle getCircle(Point a, Point b) {
+		Point c = new Point((a.x + b.x) / 2.0, (a.y + b.y) / 2.0);
+		return new Circle(c, Math.max(c.distance(a), c.distance(b)));
 	}
 
 }
